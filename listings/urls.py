6a60ad3d520de3891043bc3e4ce1e84c355c6fac
Jayani Bandara <147615801+JayaniBandara@users.noreply.github.com>
2023-12-05 15:37:34 +0530

    code error

--- listings/urls.py
+++ listings/urls.py
@@ -19,8 +19,7 @@ urlpatterns = [
     path('bidform.html', views.create_bid, name='create_bid'),  # Add this line for bidform 
     path('delete/<int:id>/', views.delete, name='delete'),
     path('bid_update/<int:id>/', views.bid_update, name='bid_update'),
-    path('logout/',views.LogoutPage,name='logout'),
-    path('login/',views.LoginPage,name='login'),
+
     
     
     path('index.html', views.index), 
